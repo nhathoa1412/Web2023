3054ed161f0e95c3553aa34128e9429e4addcd89
nhathoa1412 <139523106+nhathoa1412@users.noreply.github.com>
2023-07-31 08:26:50 +0700

    Update helloword.php

--- helloword.php
+++ helloword.php
@@ -4,5 +4,5 @@ $hello="Hello";
 $web="Web2023";
 
 echo "<h2>" .$hello." world. I'm <font color='#F00' size='+4'>" .$web."</font>";
-echo " - Lập trình <font color='#090' size='+4'>PHP1</font></h2>";
+echo " - Programming <font color='#090' size='+4'>PHP1</font></h2>";
 ?>
