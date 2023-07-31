a986c6f4878fe8bcab8ccec35383e77d684b3db4
nhathoa1412 <139523106+nhathoa1412@users.noreply.github.com>
2023-07-31 08:22:03 +0700

    Add files via upload

---
+++ helloword.php
@@ -0,0 +1,8 @@
+<?php
+
+$hello="Hello";
+$web="Web2023";
+
+echo "<h2>" .$hello." world. I'm <font color='#F00' size='+4'>" .$web."</font>";
+echo " - Lập trình <font color='#090' size='+4'>PHP1</font></h2>";
+?>
